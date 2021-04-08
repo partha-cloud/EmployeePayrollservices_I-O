95422ac9b2d24d777ec724b2cf59426e8cb3f7e4
partha-cloud <partha11dey@gmail.com>
2021-04-09 00:37:13 +0530

    added watchservice

--- test/java/com/employeepayroll/EmployeePayrollServiseTest.java
+++ test/java/com/employeepayroll/EmployeePayrollServiseTest.java
@@ -8,7 +8,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.IntStream;
 
-public class EmployeePayrollServiseTest {
+public class EmployeePayrollServiseTest
+{
     private  static String HOME = System.getProperty("user.dir");
     private  static String PLAY_WITH_NIO = "TempPlayGround";
 
@@ -34,7 +35,9 @@ public class EmployeePayrollServiseTest {
             Path tempFile = Paths.get(playPath + "/temp" + cntr);
             Assertions.assertTrue(Files.notExists(tempFile));
             try
-            { Files.createFile(tempFile); }
+            {
+                Files.createFile(tempFile);
+            }
             catch (IOException e) {}
             Assertions.assertTrue(Files.exists(tempFile));
         });
@@ -43,5 +46,15 @@ public class EmployeePayrollServiseTest {
         Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
         Files.newDirectoryStream(playPath).forEach(System.out::println);
         Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp")).forEach(System.out::println);
+    }
+
+    @Test
+    public void givenDirectoryWhenWatchedListTheActivities() throws IOException
+    {
+        Path dir = Paths.get(HOME+ "/" + PLAY_WITH_NIO);
+        Files.list(dir).filter(Files::isRegularFile)
+                .forEach(System.out::println);
+        new EmployeeWatchServices(dir).processEvents();
+
     }
 }
